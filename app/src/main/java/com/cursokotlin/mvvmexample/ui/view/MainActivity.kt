58d8a3cb542386235cb1b82b4333321b793a15f4
sarriaga35 <sebasarriaga@gmail.com>
2024-12-26 21:27:56 -0700

    Include print

--- app/src/main/java/com/cursokotlin/mvvmexample/ui/view/MainActivity.kt
+++ app/src/main/java/com/cursokotlin/mvvmexample/ui/view/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        print("Hola")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
